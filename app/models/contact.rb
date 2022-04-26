eb69e35d1a5021465d88f9800f1e037219a508a5
Paulo Henrique <ppaulo_hh@hotmail.com>
2022-04-26 15:40:45 -0300

    adicionando validates para cpf e cnpj

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,5 @@
 class Contact < ApplicationRecord
     validates :name,  length: {minimum:4}
+    validates :cpf, :cnpj, numericality: true
     validates :active, :birthday, :email, :mobile, :advertising, presence: true
 end
